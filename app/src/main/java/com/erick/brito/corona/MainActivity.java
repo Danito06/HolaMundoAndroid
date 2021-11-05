4bfaa49dd42ae518dc2d5574a6a050606f17941e
Brito Corona Erick Daniel 5IM9 <brito.corona.erick@gmail.com>
2021-11-04 18:24:05 -0600

    first commit

---
+++ app/src/main/java/com/erick/brito/corona/MainActivity.java
@@ -0,0 +1,13 @@
+package com.erick.brito.corona;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
